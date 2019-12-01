484be50e2140a5af4a74329a28062801f38e6609
Nintendroid1 <nlpsalazar@gmail.com>
2019-12-01 15:44:19 -0500

    Added employee.js which includes chef and waiter subclasses

---
+++ employee.js
@@ -0,0 +1,56 @@
+/**
+ * Employee class represent's the employee themselves
+ */
+class Employee {
+    constructor(employeeID, contactInfo) {
+      this.employeeID = employeeID; //string
+      this.contactInfo = contactInfo; //Contact class
+    }
+    get getEmployeeID() {
+        return this.employeeID;
+    }
+    set setEmployeeID(x) {
+        this.employeeID = x;
+    }
+
+    get getContactInfo() {
+        return this.contactInfo;
+    }
+    set setContactInfo(x) {
+        this.contactInfo = x;
+    }
+}
+
+/**
+ * Chef subclass represents an employee that is assigned a kitchen station
+ */
+class Chef {
+    constructor(employeeID, contactInfo, stationAssignment) {
+        Employee.call(employeeID, contactInfo);
+        this.stationAssignment = stationAssignment;
+    }
+
+    get getStationAssignment() {
+        return this.stationAssignment;
+    }
+    set setStationAssignment(x) {
+        this.stationAssignment = x;
+    }
+}
+
+/**
+ * Waiter subclass represents an employee that is assigned a table
+ */
+class Waiter {
+    constructor(employeeID, contactInfo, tableAssignment) {
+        Employee.call(employeeID, contactInfo);
+        this.tableAssignment = tableAssignment;
+    }
+
+    get getTableAssignment() {
+        return this.tableAssignment;
+    }
+    set setTableAssignment(x) {
+        this.tableAssignment = x;
+    }
+}
